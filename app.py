637f635bd26f15e477628d435dbb11bba671b6ff
ameenmanna8824 <52666434+ameenmanna8824@users.noreply.github.com>
2022-10-12 20:48:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,8 @@ op = model.predict([ip])
 if st.button('Predict'):
   st.title(op[0]) #st.button will create a button with the name Predict
   #st.title(op[0]) # the 0th element in op variable is displayed as a title
+
+  
+  
+  
+  
